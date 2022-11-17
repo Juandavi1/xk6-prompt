be264e1215b2a10286060f98c1de30ac01ba4061
Juan David Correa <juandavid5477158@hotmail.com>
2022-11-17 15:37:09 -0500

    Update sample.js

--- sample.js
+++ sample.js
@@ -6,9 +6,9 @@ export default function () {
     const options = ["smoke", "load"]
 
     // Read type from __ENV or Prompt
-    const selected = __ENV.type ? __ENV.type : prompt.select("type of test:", ...options)
-    const selected2 = __ENV.type ? __ENV.type : prompt.readInt("xd:")
-    const selected3 = __ENV.type ? __ENV.type : prompt.readString("xd2")
+    const selected = __ENV.type ? __ENV.type : prompt.select("type of test", ...options)
+    const selected2 = __ENV.type ? __ENV.VUS : prompt.readInt("total vus")
+    const selected3 = __ENV.type ? __ENV.ENV : prompt.readString("environment")
 
     // Print value
     console.log(selected, selected2, selected3)
